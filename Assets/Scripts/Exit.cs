6f86ec4d2102501d7106100650958f32be3c4edc
gab759 <gabriel.cordova.e@tecsup.edu.pe>
2025-05-30 16:26:00 -0500

    mecanica de invertir controles

--- Assets/Scripts/Exit.cs
+++ Assets/Scripts/Exit.cs
@@ -4,6 +4,9 @@ public class Exit : MonoBehaviour
 {
     public void ExitGame()
     {
-     
+        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
     }
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,32 +1,36 @@
 using System.Collections;
 using UnityEngine;
-using static UnityEngine.Rendering.DebugUI;
-
-[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
     [Header("Movement Settings")]
-    [SerializeField] private float moveSpeed = 8f;      
-    [SerializeField] private float rotationSpeed = 10f; 
-    [SerializeField] private float jumpForce = 5f;      
+    [SerializeField] private float moveSpeed = 8f;
+    [SerializeField] private float rotationSpeed = 10f;
+    [SerializeField] private float jumpForce = 5f;
 
     private Rigidbody rb;
     private Vector3 moveDirection;
     private bool isGrounded;
-   // [SerializeField] private float TimeBuff;
+
+    private bool isControlsInverted = false;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.freezeRotation = true; 
+        rb.freezeRotation = true;
     }
 
     void Update()
     {
-      
         float horizontalInput = Input.GetAxisRaw("Horizontal");
         float verticalInput = Input.GetAxisRaw("Vertical");
 
-    
+        // Invertir controles si el debuff está activo
+        if (isControlsInverted)
+        {
+            horizontalInput *= -1;
+            verticalInput *= -1;
+        }
+
         moveDirection = new Vector3(horizontalInput, 0f, verticalInput).normalized;
 
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
@@ -38,13 +42,9 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-      
         if (moveDirection != Vector3.zero)
         {
-            
             rb.MovePosition(rb.position + moveDirection * moveSpeed * Time.fixedDeltaTime);
-
-            
             Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
             rb.rotation = Quaternion.Slerp(rb.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime);
         }
@@ -52,21 +52,36 @@ public class PlayerController : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
-      
-        if (collision.gameObject.tag == ("Ground"))
+        if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
         }
     }
-    public  void StartLento(float value, int     timebuff)
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("debuff1")) // Debuff: invertir controles
+        {
+            StartCoroutine(InvertControlsForSeconds(5f));
+        }
+    }
+
+    public void StartLento(float value, int timebuff)
     {
         StartCoroutine(Lento(value, timebuff));
     }
+
     public IEnumerator Lento(float value, int time)
     {
         float originalMoveSpeed = moveSpeed;
-        moveSpeed = value; 
-        yield return new WaitForSeconds(time); 
-        moveSpeed = originalMoveSpeed; 
+        moveSpeed = value;
+        yield return new WaitForSeconds(time);
+        moveSpeed = originalMoveSpeed;
+    }
+
+    public IEnumerator InvertControlsForSeconds(float seconds)
+    {
+        isControlsInverted = true;
+        yield return new WaitForSeconds(seconds);
+        isControlsInverted = false;
     }
 }
